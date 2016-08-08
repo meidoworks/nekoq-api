bc327e30a847e8875b78a37755e49af979a3eeb6
goodplayer <meidomx@gmail.com>
2016-08-08 20:41:49 +0800

    context

--- context/context.go
+++ context/context.go
@@ -1,7 +1,15 @@
 package context
 
+type Context struct {
+	TraceId string
+	RpcId   string
+	Data    map[string]string
+	AppName string
+}
+
 type RpcContext struct {
 	TraceId string
 	RpcId   string
 	Data    map[string]string
+	AppName string
 }
