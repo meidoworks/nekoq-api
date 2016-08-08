2aab6fd35fa45e715b607090dd52874a186e6c7b
goodplayer <meidomx@gmail.com>
2016-08-08 22:39:47 +0800

    client manager

--- rpc/client.go
+++ rpc/client.go
@@ -2,14 +2,24 @@ package rpc
 
 import (
 	"import.moetang.info/go/nekoq-api/context"
+	"import.moetang.info/go/nekoq-api/errors"
 )
 
-type Client interface {
-	Call(context *context.RpcContext, request interface{}, response interface{}) error
-	CallWithReturning(context *context.RpcContext, request interface{}) (interface{}, error)
+type Param struct {
+	Method  string
+	Request interface{}
+	Context *context.RpcContext
 }
 
-func NewClient(name string) Client {
-	//TODO
-	return nil
+type Client interface {
+	Call(param Param, response interface{}) error
+	CallWithReturning(param Param) (interface{}, error)
+}
+
+func NewClient(name string) (Client, error) {
+	c, ok := clientImplMap[name]
+	if ok {
+		return c, nil
+	}
+	return nil, errors.New("nekoq-api: no rpc client mapped of " + name)
 }
--- rpc/manager.go
+++ rpc/manager.go
@@ -1,1 +1,28 @@
 package rpc
+
+import (
+	"sync"
+	"sync/atomic"
+	"unsafe"
+)
+
+var (
+	clientImplMap    = make(map[string]Client)
+	clientLock       = &sync.Mutex{}
+	tmpClientImplMap map[string]Client
+)
+
+func RegisterClientImpl(name string, client Client) {
+	clientLock.Lock()
+	// must be assigned to a global field
+	tmpClientImplMap = make(map[string]Client)
+	for k, v := range clientImplMap {
+		tmpClientImplMap[k] = v
+	}
+	tmpClientImplMap[name] = client
+	orgiPtr := (*uintptr)(unsafe.Pointer(&clientImplMap))
+	newPtr := (*uintptr)(unsafe.Pointer(&tmpClientImplMap))
+	atomic.StoreUintptr(orgiPtr, *newPtr)
+	tmpClientImplMap = nil
+	clientLock.Unlock()
+}
