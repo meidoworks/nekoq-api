079f6e09939466d747067fdbf0a537f2515ed812
goodplayer <meidomx@gmail.com>
2016-08-10 21:55:38 +0800

    provide function to force check appname and node flag

--- env/flag.go
+++ env/flag.go
@@ -15,6 +15,9 @@ func init() {
 	flag.StringVar(&appname, "appname", "", "AppName of application. e.g. -appname=nekoq")
 	flag.StringVar(&nodeid, "node", "", "Unique Node Id of application. e.g. -node=nekoq001")
 	flag.Parse()
+}
+
+func EnsureEnvFlag() {
 	if appname == "" {
 		fmt.Fprintln(os.Stderr, "Please set AppName using flag '-appname'.")
 		os.Exit(-100)
