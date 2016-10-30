4c231abfa2ff80b10b026e616ae69895746b2b97
goodplayer <meidomx@gmail.com>
2016-10-30 18:40:43 +0800

    server api

--- rpc/server/server.go
+++ rpc/server/server.go
@@ -1,4 +1,37 @@
 package rpc
 
+import (
+	"sync"
+
+	"import.moetang.info/go/nekoq-api/errorutil"
+)
+
+var serverFactories = make(map[string]ServerFactory)
+var mLock = &sync.Mutex{}
+
 type ServerFactory interface {
+	Start() error
+	Stop() error
+}
+
+func RegisterServerFactory(serviceName string, serverFactory ServerFactory) error {
+	mLock.Lock()
+	_, ok := serverFactories[serviceName]
+	if ok {
+		mLock.Unlock()
+		return errorutil.New("same server name has registered: " + serviceName)
+	}
+	serverFactories[serviceName] = serverFactory
+	mLock.Unlock()
+	return nil
+}
+
+func StartAllServices() error {
+	for k, v := range serverFactories {
+		err := v.Start()
+		if err != nil {
+			return errorutil.NewNested("start service error: "+k, err)
+		}
+	}
+	return nil
 }
